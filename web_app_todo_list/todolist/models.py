7344f8a298140f2bc227965dff852372e41b4b55
TmSalviano <TiagoMSalviano@outlook.com>
2025-11-23 22:26:31 -0300

    creating superuser and registering models

--- web_app_todo_list/todolist/models.py
+++ web_app_todo_list/todolist/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 class TodoItem(models.Model):
-    summary = models.CharField(100)
-    todo_text = models.TextField(verbose_name="todo text")
+    summary = models.CharField(max_length=100)
+    todo_text = models.TextField()
     pub_date = models.DateTimeField(verbose_name="date published")
+    done = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.summary
